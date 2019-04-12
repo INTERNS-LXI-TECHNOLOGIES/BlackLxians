84d266849b998a5c9fc6a7a4a687f42ee93d9be0
ayana98 <ayana.k.u@lxisoft.com>
2019-04-12 16:06:35 +0530

    LI-509

--- Quiz_v_1.1/com/blacklxians/quiz/controller/LogginController.java
+++ Quiz_v_1.1/com/blacklxians/quiz/controller/LogginController.java
@@ -7,7 +7,7 @@ public class LogginController
 	Properties p=new Properties();
 public String validation(String userName,String password)
 {
-	String result=null;
+	String result="invalid";
 	try
 	{
 	
@@ -18,14 +18,19 @@ public String validation(String userName,String password)
 if(userName.equals(p.getProperty("ausername")) && password.equals(p.getProperty("apassword")))
 {
 result="admin loggin";
-
 }
-else if(userName.equals(p.getProperty("username")) && password.equals(p.getProperty("password")))
+else
 {
-result="user loggin";
+//result="user loggin";
+int count=count();
+	count=count-2;
+	count=count/4;
+for(int i=0;i<count;i++)
+{
+	if(userName.equals(p.getProperty("username"+i)) && password.equals(p.getProperty("password"+i)))
+	   result="user loggin";
+   
 }
-else{
-result="invalid";
 }
 	}
 catch(IOException e)
@@ -39,12 +44,15 @@ return result;
 public String registerValidation(String userName,String password)
 {
 	String res=null;
+	int count=count();
+	count=count-2;
+	count=count/4;
 	try{
 	
 	//FileWriter fw=new FileWriter("db.properties");
 	//BufferedWriter bw=new BufferedWriter(fw);
-	p.setProperty("username=",userName);
-	p.setProperty("password=",password);
+	p.setProperty("username"+count,userName);
+	p.setProperty("password"+count,password);
 	p.store(new FileWriter("db.properties",true),"");
 	}
 	catch(IOException e)
@@ -55,6 +63,22 @@ public String registerValidation(String userName,String password)
 	
 }
 
+public int count()
+{
+	int count=0;
+	try{
+		FileReader fr=new FileReader(new File("db.properties"));
+		BufferedReader br=new BufferedReader(fr);
+		String data;
+		while((data=br.readLine())!=null)
+		{
+			count++;
+		}
+	}
+	catch(Exception e){
+	}
 
+return count;
 }
 
+}
